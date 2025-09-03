98c8a16b5d2d2c1e47bc92810a8173ec0944505d
goncho09 <gonzalolarricaV2@gmail.com>
2025-09-03 13:38:21 -0300

    guardado datos

--- js/almacenar.js
+++ js/almacenar.js
@@ -3,12 +3,33 @@ const inputItem = document.getElementById('item');
 const contenedor = document.getElementById('contenedor');
 const listElement = [];
 
+function init() {
+  contenedor.innerHTML = JSON.parse(localStorage.getItem('elementos') || '[]')
+    .map((listElement) => `<li>${listElement}</li>`)
+    .join('');
+}
+
+init();
+
 agregarButton.addEventListener('click', () => {
-    listElement.push(inputItem.value);
-    updateListContainer(listElement);
-    inputItem.value = '';
+  listElement.push(inputItem.value);
+  updateListContainer(listElement);
+  inputItem.value = '';
+  guardarDatos();
 });
 
 function updateListContainer(listElement) {
-    contenedor.innerHTML = listElement.map(item => `<li>${item}</li>`).join('')
+  contenedor.innerHTML += listElement
+    .map((listElement) => `<li>${listElement}</li>`)
+    .join('');
+}
+
+function guardarDatos() {
+  localStorage.setItem(
+    'elementos',
+    JSON.stringify([
+      ...JSON.parse(localStorage.getItem('elementos') || '[]'),
+      ...listElement,
+    ])
+  );
 }
